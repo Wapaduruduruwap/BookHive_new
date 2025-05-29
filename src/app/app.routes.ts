fa7f30b330e492dd550e57008a270535738c494d
Wapaduruduruwap <potaninaa2005@mail.ru>
2025-05-29 12:37:20 +0500

    fдобавлен модуль и компонент профиля с настройкой маршрутизации

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'profile',
+    loadChildren: () => import('./features/profile/profile-module').then(m => m.ProfileModule)
+  }
+];
