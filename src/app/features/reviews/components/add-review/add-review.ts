d9a99c7893119e41dbb04db58bd4215f60ad2fb5
Wapaduruduruwap <potaninaa2005@mail.ru>
2025-06-01 13:51:02 +0500

    Настроены шаблон и стили для компонентов списка рецензий и их добавления

--- src/app/features/reviews/components/add-review/add-review.ts
+++ src/app/features/reviews/components/add-review/add-review.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-add-review',
@@ -15,10 +16,34 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule
   ],
   templateUrl: './add-review.html',
   styleUrls: ['./add-review.scss']
 })
-export class AddReviewComponent {
+export class AddReviewComponent implements OnInit {
+  reviewForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.reviewForm = this.fb.group({
+      bookTitle: ['', Validators.required],
+      author: ['', Validators.required],
+      rating: [0, [Validators.required, Validators.min(1), Validators.max(5)]],
+      text: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {}
+
+  setRating(rating: number): void {
+    this.reviewForm.patchValue({ rating });
+  }
+
+  onSubmit(): void {
+    if (this.reviewForm.valid) {
+      console.log(this.reviewForm.value);
+      // TODO: Добавить сохранение рецензии
+    }
+  }
 }
